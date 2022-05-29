8b03bf3f01c9632f7f21772cd8f0620fa07dc610
Danny <khc741213+1qaz@gmail.com>
2022-05-29 10:18:53 +0800

    feat:增加 集合、字典的基本運算

--- 4.py
+++ 4.py
@@ -1,13 +1,36 @@
 # Python 有序列表的基本運算 - List、Tuple
 
 # 有序可變動列表 List
+from cgitb import grey
+
+
 grades=[12,60,15,70,90]
-grades[0]=55
-print(grades)
+# grades[0]=55 # 取得特定元素
+# grades[1:4]=[]  # 連續刪除列表元素
+
+# grades=grades+[12,33] #列表串接
+
+# print(grades)
+
+# print(grades[1:4])
 
 # print(grades[0])
 
+# lenth=len(grades)#取得列表長度
+# print(lenth)
+
+# data=[[3,4,5],[6,7,8]]
+# print(data[0][0])
+
+# data[0][0:2]=[5,5,5]
+# print(data[0])
+# print(data)
+
 # 有序不可變動列表 Tuple
 
-
+data=(3,4,5)
+data[0]=5
+# print(data[2])
+# print(data[0:2])
+print(data)
 
